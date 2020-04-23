e9fddec33a7542ef172169d9e80e48dabe0c65b5
STUTuna <s15113114@stu.edu.tw>
2020-04-23 19:07:29 +0800

    Update autoFinish.js

--- autoFinish.js
+++ autoFinish.js
@@ -1,5 +1,4 @@
 document.addEventListener("keyup", function(event) {
-    document.getElementById('inputfield');
     let answer = document.getElementsByClassName('highlight')[0].textContent;
     inputfield.value = answer;
 });
